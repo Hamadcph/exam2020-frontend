a254e3cd570e5a58ce539f381f01721c7c460e2d
Hamad <cph-ss451@cphbusiness.dk>
2020-01-13 18:13:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,171 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch
+} from "react-router-dom";
+import facade from "./apiFacade";
+import DND from "./components/DND";
+import Home from "./components/Home";
+import {
+  Button,
+  InputGroup,
+  FormControl,
+  Form,
+  Container
+} from "react-bootstrap";
+
+class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "" };
+  }
+  login = evt => {
+    evt.preventDefault();
+    this.props.login(this.state.username, this.state.password);
+  };
+  onChange = evt => {
+    this.setState({ [evt.target.id]: evt.target.value });
+  };
+  render() {
+    return (
+      <Container>
+        <Form onSubmit={evt => this.login(evt)}>
+          <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              onChange={this.onChange}
+              placeholder="Enter username"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              onChange={this.onChange}
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
+}
+class LoggedIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dataFromServer: "Fetching!!" };
+  }
+  componentDidMount() {
+    facade.fetchData().then(res => this.setState({ dataFromServer: res.msg }));
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <Content />
+      </div>
+    );
+  }
+}
+/*
+function LoggedIn() {
+  const [data, setData] = useState({});
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    facade.fetchSpell(id).then(res => setData(res));
+    //facade.fetchData().then(res => setData(res));
+  }, [id]);
+
+  function set_Id(evt) {
+    const id = document.getElementById("id").value;
+    setId(id);
+  }
+
+  return (
+    <div>
+      <h2>Data Received from server</h2>
+      <div>
+        <Header />
+        <input id="id" type="text" placeholder="Search" />
+        <button onClick={set_Id}> Search </button>
+      </div>
+      <h3>{JSON.stringify(data)}</h3>
+      <Content />
+    </div>
+  );
+}
+*/
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+  }
+  logout = () => {
+    facade.logout();
+    this.setState({ loggedIn: false });
+  }; //TODO
+  login = (user, pass) => {
+    facade.login(user, pass).then(res => this.setState({ loggedIn: true }));
+  }; //TODO
+  render() {
+    return (
+      <Container>
+        {!this.state.loggedIn ? (
+          <LogIn login={this.login} />
+        ) : (
+          <Router>
+            <div>
+              <LoggedIn />
+              <Button variant="secondary" onClick={this.logout}>
+                Logout
+              </Button>
+            </div>
+          </Router>
+        )}
+      </Container>
+    );
+  }
+}
+export default App;
+
+const Header = () => {
+  return (
+    <ul className="header">
+      <li>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/DND">
+          Dungeons and Dragon
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+const Content = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        {" "}
+        <Home />{" "}
+      </Route>
+      <Route path="/DND">
+        {" "}
+        <DND />{" "}
+      </Route>
+    </Switch>
+  );
+};
